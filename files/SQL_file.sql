8a1a119b7af7a9a1666d742705d51ca2ef17015e
dylan kessler <dylnksslr53@gmail.com>
2020-04-09 11:41:03 -0400

    completed all of the basic functionality; it creates a basic CRUD SB project for a single table that passes all Unit tests and starts without issues

--- files/SQL_file.sql
+++ files/SQL_file.sql
@@ -1,14 +1,1 @@
-# groups table - basic members, premium members, admins, etc...
-# each user can only belong to one group_type at a time
-create table if not exists groups(grp_id mediumint not null auto_increment primary key,
-grp_name varchar(30) comment "brief name of the group",
-grp_desc varchar(256) comment "group or membership type description");
-
-# groups_hist - history table for group
-create table if not exists groups_hist(grp_hist_id mediumint not null auto_increment primary key,
-grp_id mediumint not null comment "FK to group.group_id",
-grp_name varchar(30) comment "brief name of the group",
-grp_desc varchar(256) comment "group or membership type description",
-action_type varchar(5) comment "what action was performed on this record?",
-evnt_tmestmp datetime comment "when did this action happen?",
-evnt_oper_id varchar(256) comment "who did this action?, FK to the users.username field");
+< put your SQL create statements here >
