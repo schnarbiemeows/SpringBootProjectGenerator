da1779385902c19478d773a0e48131a2ec141c4a
schnarbiemeows <dylnksslr53@gmail.com>
2025-03-14 20:39:17 -0400

    updated code to handle Angular 19

--- configuration/Configuration.py
+++ configuration/Configuration.py
@@ -194,10 +194,9 @@ class Configuration:
     app_hib_nmg = "spring.jpa.hibernate.naming.physical-strategy=org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl"
     app_hib_seq = "spring.jpa.hibernate.use-new-id-generator-mappings=false"
     # !!!! - when running on an EC2, we need '?enabledTLSProtocols=TLSv1.2' appended to the jdbc URL now
-    app_mysql_conn = "spring.datasource.url=jdbc:mysql://restoredmysql.cb5hn7nsfqoo.us-east-1.rds.amazonaws.com:3306/nms-accounts?enabledTLSProtocols=TLSv1.2"
-    #app_mysql_conn = "spring.datasource.url=jdbc:mysql://firstmysql-5-7-16.cb5hn7nsfqoo.us-east-1.rds.amazonaws.com:3306/nms-accounts?enabledTLSProtocols=TLSv1.2"
-    app_mysql_usr = "spring.datasource.username=schnarbies"
-    app_mysql_pwd = "spring.datasource.password=NYCis#1ontoNYC!"
+    app_mysql_conn = "spring.datasource.url=jdbc:mysql://locahost:3306/<schemaname>"
+    app_mysql_usr = "spring.datasource.username=username"
+    app_mysql_pwd = "spring.datasource.password=password"
     app_jpa_show = "spring.jpa.show-sql=true"
     app_actu_conf = "management.endpoints.web.exposure.include=*"
     app_sec_usr = "spring.security.user.name=test"
